c0b071d9f1b96ecc3a097d7654cfb32e03c94c61
nsdevx4978 <nsdevx@gmail.com>
2023-06-01 13:09:29 +0100

    Add Extensions

---
+++ FlickrDemo/Extensions/ViewExt.swift
@@ -0,0 +1,23 @@
+//
+//  ViewExt.swift
+//  FlickrDemo
+//
+//  Created by ervinaydin on 01/06/2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension View {
+    func photoHeaderStyle() -> some View {
+        self.modifier(PhotoHeaderStyleModifier())
+    }
+    
+    func h1TitleStyle() -> some View {
+        self.modifier(H1TitleStyleModifier())
+    }
+    
+    func shouldHide(_ flag: Bool) -> some View {
+        self.modifier(ViewHideModifier(flag: flag))
+    }
+}
