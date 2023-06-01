2ad6f9c58caaed9b99859d39b3780c8fed6109e4
nsdevx4978 <nsdevx@gmail.com>
2023-06-01 12:20:04 +0100

    Initial Commit

---
+++ FlickrDemo/FlickrDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrDemoApp.swift
+//  FlickrDemo
+//
+//  Created by ervinaydin on 01/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlickrDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
