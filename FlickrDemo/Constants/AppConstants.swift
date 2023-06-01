64670f88258c2445f8bce73228d83fb5dee3294b
nsdevx4978 <nsdevx@gmail.com>
2023-06-01 13:05:15 +0100

    Add Constants

---
+++ FlickrDemo/Constants/AppConstants.swift
@@ -0,0 +1,16 @@
+//
+//  AppConstants.swift
+//  FlickrDemo
+//
+//  Created by ervinaydin on 01/06/2023.
+//
+
+import Foundation
+
+struct AppConstants {
+    
+    struct PageTitle {
+        static let PhotoListView   = "YBS App"
+        static let PhotoDetailView = "Details"
+    }
+}
